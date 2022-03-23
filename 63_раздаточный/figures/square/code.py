ad6e5609f20d6db835c6cb04901e37c8be9e612f
OksanaPod31 <91842016+OksanaPod31@users.noreply.github.com>
2022-03-23 17:29:37 +0300

    Add files via upload

---
+++ 63_раздаточный/figures/square/code.py
@@ -0,0 +1,9 @@
+__all__ = ['squre_perimetr', 'squre_area']
+
+
+def squre_perimetr(a=15):
+    return print('Периметр квадрата: ', 4 * a)
+
+
+def squre_area(a=15):
+    return print('Площадь квадрата', a ** 2)
