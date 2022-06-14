36973b7b18ff6d8c40123b059d904b76fbc6c42c
OksanaPod31 <91842016+OksanaPod31@users.noreply.github.com>
2022-06-14 17:46:31 +0300

    Delete test directory

--- test/five.py
+++
@@ -1,8 +0,0 @@
-def power(b, e):
-    if (e == 1):
-        return (b)
-    if (e != 1):
-        return (b * power(b, e - 1))
-b = int(input("Введите число: "))
-e = int(input("Введите степень: "))
-print("Результат:", power(b, e))
--- test/test2/zadaha.py
+++
@@ -1,9 +0,0 @@
-
-def Ten(n):
-    if n > 1:
-        res = Ten(n // 2) + str(n % 2)
-        return res
-    else:
-        return '1'
-
-print("Двоичное число", Ten(13))
